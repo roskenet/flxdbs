b45c711039e06086809b70a63124f2d1275cf102
Felix Roske <felix@zalando.de>
2023-07-03 08:47:22 +0200

    Sync

--- postgres.sql
+++ postgres.sql
@@ -1,6 +1,9 @@
---| General PostgreSQL maintenance scripts |-------------------------------------------
+--| General PostgreSQL maintenance scripts |------------------------------------
 SELECT * FROM pg_database;
 
+CREATE ROLE :ROLE WITH LOGIN PASSWORD :PASSWORD;
 CREATE DATABASE :DBNAME OWNER :OWNER;
 
 SELECT * FROM pg_roles;
+
+-- DROP DATABASE :DBNAME;
